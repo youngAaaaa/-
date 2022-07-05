613feb137afaf55aa98f52cee9105b1bc37cdf16
youngAaaaa <97266875+youngAaaaa@users.noreply.github.com>
2022-07-05 10:35:08 +0900

    프로그래머스 Lv.1 신규 아이디 추천 (220705)

---
+++ 신규 아이디 추천.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+func solution(_ new_id:String) -> String {
+    //1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+    var id = new_id.lowercased()
+    
+    //2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    id = id.filter {
+        $0.isLowercase || $0.isNumber || $0 == "-" || $0 == "_" || $0 == "."
+    }
+    
+    //3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    while id.contains(".."){
+        id = id.replacingOccurrences(of: "..", with: ".")
+    }
+    
+    //4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    if id.first == "."{
+        id.removeFirst()
+    }
+    if id.last == "." {
+        id.removeLast()
+    }
+    
+    //5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    if id.isEmpty { id.append("a") }
+    
+    
+    //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    if id.count >= 16 {
+        id = String(id.prefix(15))
+        //만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+        while id.last == "." {
+            id.removeLast()
+        }
+    }
+    
+    //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+    while id.count < 3{
+        if let last = id.last{
+            id.append(last)
+        }
+    }
+    
+    return id
+}
+
+//print(solution("...!@BaT#*..y.abcdefghijklm"))
+//print(solution("z-+.^."))
+//print(solution("=.="))
+//print(solution("123_.def"))
+//print(solution("abcdefghijklmn.p"))
+print(solution("......a......a......a....."))
