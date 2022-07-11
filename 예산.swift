7c56247de81bfd1ba36059a35227472f1acf7c50
youngAaaaa <97266875+youngAaaaa@users.noreply.github.com>
2022-07-12 00:06:20 +0900

    프로그래머스 Lv.1 예산 (220711)

--- 예산.swift
+++ 예산.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 func solution(_ d:[Int], _ budget:Int) -> Int {
-    var sort_d = d
-    sort_d.sort()
+    var sortD = d
+    sortD.sort()
     
     var sum = budget
     var cnt = 0
     
-    for buseo in sort_d{
+    for buseo in sortD{
         if sum - buseo < 0 { break }
         sum -= buseo
         cnt += 1
